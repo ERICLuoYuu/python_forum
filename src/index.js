f05b180a0feb5918cd52a27e9d5d96e70f28ed73
ERICLuoYuu <YU.LUO@UNI-MUENSTER.DE>
2024-11-20 11:11:02 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
